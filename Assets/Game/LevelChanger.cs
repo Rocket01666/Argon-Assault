3eae8789c61b9d5199c51b1109f9e68aa85a4f19
Rocket01666 <rocket01666@live.ca>
2019-01-23 22:22:47 -0500

    creating fade in and fade out, switched from VS community to VS code. working on creating delay for scene change script has an error that needs to be resolved removed old skybox and replaced it with a new one, added development packages for ECS and Burst compiler

---
+++ Assets/Game/LevelChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update(){
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay(){
+        yield return new WaitForSeconds(1);
+    }
+
+    public void FadeToLevel(int levelIndex){
+        levelToLoad + levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete(){
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
