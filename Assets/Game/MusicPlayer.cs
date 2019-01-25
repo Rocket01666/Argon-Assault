3b47ef9d4c7f5391b9853d97e8bfda0f5fba0124
Rocket01666 <rocket01666@live.ca>
2019-01-25 14:37:20 -0500

    removed 2 scripts, created a new script to change the level from splash to lvl1, modified the Assembly-CSharp.csproj  the targeting .net framework should be 4.7.2

---
+++ Assets/Game/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene(){
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
