85441675a277acb58ac42cd0092044575e79380b
Rocket01666 <rocket01666@live.ca>
2019-03-21 22:01:25 -0400

    Organized folders, made things more clear and easier to locate, Created a Crosshair, created Crosshair script. bug crosshair becomes invisible from game space or gets stuck in the center of the screen

---
+++ Argon-Assault/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Crosshair : MonoBehaviour
+{
+    public GameObject particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = Input.mousePosition;
+
+          if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray))
+                Instantiate(particle, transform.position, transform.rotation);
+        }
+    }
+
+}
